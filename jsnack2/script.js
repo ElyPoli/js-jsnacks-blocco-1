4c73cc882c2fa995ca571577adbe9a1edff6a89a
ElyPoli <itsos.poli@gmail.com>
2023-07-06 19:48:48 +0200

    added user interaction - ex. Snack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,15 +5,47 @@ Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in 
 
 // Dichiaro variabili e creo una lista di numeri interi
 let sum = 0;
-const intNumber = [99, 77, 97, 82, 80, 66, 8, 83, 73, 42, 57, 33, 6, 68, 11, 74, 32, 86];
+const intNumber = [];
+const btnDelete = document.querySelector(".btn-delete");
+const btnCreate = document.querySelector(".btn-create");
+const numberOfListElement = document.querySelector(".number-of-list");
+const listCreate = document.querySelector(".list-create");
 
-// Scorro su tutta la lista "saltando" ogni volta di 2
-for (let i = 0; i < intNumber.length; i = i + 2) {
+// Creo un event listener con il pulsante "Genera" per chiedere all'utente quanti numeri vuole nella lista
+btnCreate.addEventListener("click", function () {
 
-    console.log(intNumber[i]);
+    const numberOfList = numberOfListElement.value;
 
-    // Mentre scorro sommo i numeri
-    sum += intNumber[i];
-}
+    // Controllo che il numero inserito sia corretto
+    if (numberOfList <= 0) {
+        alert("Hai inserito un valore errato.");
+        location.reload();
+    }
 
-console.log(sum);
+    // Aggiungo alla lista il numero di elementi scelto dall'utente
+    for (let i = 0; i < numberOfList; i++) {
+        intNumber.push(Math.floor(Math.random() * (99 - 1 + 1)) + 1);
+
+        // Mostro all'utente la lista creata
+        listCreate.classList.add("border", "rounded-4");
+        listCreate.innerHTML += `<li class="list-group-item">${intNumber[i]}</li>`;
+    }
+
+    // Scorro su tutta la lista "saltando" ogni volta di 2
+    for (let i = 0; i < intNumber.length; i = i + 2) {
+
+        // Mostro all'utente quali sono gli elementi in posizione dispari
+        listCreate.innerHTML += `<li class="list-group-item bg-secondary text-white">${intNumber[i]} è in posizione dispari</li>`;
+
+        // Mentre scorro sommo i numeri
+        sum += intNumber[i];
+    }
+
+    // Mostro all'utente il totale degli elementi in posizione dispari
+    listCreate.innerHTML += `<li class="list-group-item bg-success text-white">La somma dei numeri in posizione dispari è: ${sum}</li>`;
+})
+
+// Creo un event listener con il pulsante "Cancella" per resettare la lista
+btnDelete.addEventListener("click", function () {
+    location.reload();
+})
